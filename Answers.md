c9ef2ecf6d219cc2a4ce9f81e7cfa121bf12508d
Kaili Kleinschmidt <kkleinschmidt@luc.edu>
2017-11-19 22:32:55 +0000

    fixing github issues
    Signed-off-by: Kaili Kleinschmidt <kkleinschmidt@luc.edu>

---
+++ Answers.md
@@ -0,0 +1,14 @@
+### Try using a `TreeMap` and a `HashMap` instead of `MyHashMap`. Are the resulting word frequencies any different? Is the time performance any different? If so, how would you rank the three implementations (in increasing order of time complexity)?
+
+
+
+### How are `%` and `Math.floorMod` different? Which works more reliably for computing a hash table index?
+
+math.floormod would work better for this case, because it knows how to deal with negative numbers, and not return them, whereas Modulo alone cannot do that.
+
+### What is the time complexity of `MyHashMap.size()`, and how could you make it much more efficient?
+
+
+
+### How does this implementation compare to one where you would directly use your linked `Node` class from the earlier assignment? Answer briefly in terms of ease of implementation, correctness, reliability, and performance.
+
---
+++ src/main/java/edu/luc/cs271/myhashmap/MyHashMap.java
@@ -0,0 +1,208 @@
+package edu.luc.cs271.myhashmap;
+
+import java.util.*;
+
+/**
+ * A generic HashMap custom implementation using chaining. Concretely, the table is an ArrayList of
+ * chains represented as LinkedLists.
+ *
+ * @param <K> the key type
+ * @param <V> the value type
+ */
+public class MyHashMap<K, V> implements Map<K, V> {
+
+  private static final int DEFAULT_TABLE_SIZE = 11; // a prime
+
+  private List<List<Entry<K, V>>> table;
+
+  public MyHashMap() {
+    this(DEFAULT_TABLE_SIZE);
+  }
+
+  public MyHashMap(final int tableSize) {
+    // allocate a table of the given size
+    table = new ArrayList<>(tableSize);
+    // then create an empty chain at each position
+    for (int i = 0; i < tableSize; i += 1) {
+      table.add(new LinkedList<>());
+    }
+  }
+
+  @Override
+  public int size() {
+    // DONE ? add the sizes of all the chains
+    int result = 0;
+    
+    for(int i = 0; i < table.size(); i++){
+      for(int j = 0; j < table.get(i).size(); j++){
+        result++;
+      }
+    }
+
+    return result;
+  }
+
+  @Override
+  public boolean isEmpty() {
+    return size() == 0;
+  }
+
+  @Override
+  public boolean containsKey(final Object key) {
+    // DONE follow basic approach of remove below (though this will be much simpler)
+    final int index = calculateIndex(key);
+    
+    final Iterator<Entry<K, V>> iter = table.get(index).iterator();
+    while (iter.hasNext()) {
+      final Entry<K, V> entry = iter.next();
+      if (entry.getKey().equals(key)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  @Override
+  public boolean containsValue(final Object value) {
+    // DONE ? follow basic approach of remove below (though this will be much simpler)
+    
+    final int index = calculateIndex(value);
+    final Iterator<Entry<K, V>> iter = table.get(index).iterator();
+    while (iter.hasNext()) {
+      final Entry<K, V> entry = iter.next();
+      if (entry.getValue().equals(value)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  @Override
+  public V get(final Object key) {
+    // DONE follow basic approach of remove below (though this will be simpler)
+    final int index = calculateIndex(key);
+
+    final Iterator<Entry<K, V>> iter = table.get(index).iterator();
+    while (iter.hasNext()) {
+      final Entry<K, V> entry = iter.next();
+      if (entry.getKey().equals(key)) {
+        final V oldValue = entry.getValue();
+        return oldValue;
+      }
+    }
+    
+    return null;
+  }
+
+  @Override
+  public V put(final K key, final V value) {
+    // DONE ? follow basic approach of remove below (this will be similar)
+    final int index = calculateIndex(key);
+
+    final Iterator<Entry<K, V>> iter = table.get(index).iterator();
+    while (iter.hasNext()) {
+      final Entry<K, V> entry = iter.next();
+      if (entry.getKey().equals(key)) {
+        final V oldValue = entry.getValue();
+        entry.setValue(value);
+        return oldValue;
+      }
+    }
+    
+    return null;
+  }
+
+  @Override
+  public V remove(final Object key) {
+    final int index = calculateIndex(key);
+    final Iterator<Entry<K, V>> iter = table.get(index).iterator();
+    while (iter.hasNext()) {
+      final Entry<K, V> entry = iter.next();
+      if (entry.getKey().equals(key)) {
+        final V oldValue = entry.getValue();
+        iter.remove();
+        return oldValue;
+      }
+    }
+    return null;
+  }
+
+  @Override
+  public void putAll(final Map<? extends K, ? extends V> m) {
+    // DONE ? add each entry in m's entrySet
+    int numKeysToBeAdded = m.size();
+    if (numKeysToBeAdded == 0){
+      return;
+    }
+
+   for (Iterator<? extends Map.Entry<? extends K, ? extends V>> i = m.entrySet().iterator(); i.hasNext(); ) {
+       Map.Entry<? extends K, ? extends V> e = i.next();
+       put(e.getKey(), e.getValue());
+   }
+
+  }
+
+  @Override
+  public void clear() {
+    // DONE clear each chain
+    
+    for (int i = 0; i < table.size(); i += 1) {
+      table.remove(i);
+    }
+
+  }
+
+  /** The resulting keySet is not "backed" by the Map, so we keep it unmodifiable. */
+  @Override
+  public Set<K> keySet() {
+    final Set<K> result = new HashSet<>();
+    // TODO populate the set f
+
+    return Collections.unmodifiableSet(result);
+  }
+
+  /** The resulting values collection is not "backed" by the Map, so we keep it unmodifiable. */
+  @Override
+  public Collection<V> values() {
+    final List<V> result = new LinkedList<>();
+    // TODO populate the list
+
+
+    return Collections.unmodifiableCollection(result);
+  }
+
+  /** The resulting entrySet is not "backed" by the Map, so we keep it unmodifiable. */
+  @Override
+  public Set<Entry<K, V>> entrySet() {
+    final Set<Entry<K, V>> result = new HashSet<>();
+    // TODO populate the set
+
+
+    return Collections.unmodifiableSet(result);
+  }
+
+  @Override
+  public String toString() {
+    // TODO return the string representation of the underlying table
+    return "";
+  }
+
+  public boolean equals(final Object that) {
+    if (this == that) {
+      return true;
+    } else if (!(that instanceof Map)) {
+      return false;
+    } else {
+      // TODO simply compare the entry sets
+      return false;
+    }
+  }
+
+  private int calculateIndex(final Object key) {
+    // positive remainder (as opposed to %)
+    // required in case hashCode is negative!
+    return Math.floorMod(key.hashCode(), table.size());
+  }
+}
