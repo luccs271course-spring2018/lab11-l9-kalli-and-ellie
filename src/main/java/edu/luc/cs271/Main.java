9d8e27320ada698b1865bc2f6398b16511ef22e6
Kaili Kleinschmidt <kkleinschmidt@luc.edu>
2017-12-05 20:58:51 +0000

    initial commit
    Signed-off-by: Kaili Kleinschmidt <kkleinschmidt@luc.edu>

---
+++ src/main/java/edu/luc/cs271/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+
+  public static void main(final String[] args) throws InterruptedException {
+    
+    final Scanner input = new Scanner(System.in).useDelimiter("[^\\p{Alnum}]+");
+
+    
+  }
+}
